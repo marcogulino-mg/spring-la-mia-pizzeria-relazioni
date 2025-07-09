9337735c8d2c811bf036f5335f4504617f21565b
marcogulino-mg <marcogulino@gmail.com>
2025-07-09 16:18:56 +0200

    first commit

---
+++ src/test/java/org/java/lesson/exercise/spring_la_mia_pizzeria_relazioni/SpringLaMiaPizzeriaRelazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.lesson.exercise.spring_la_mia_pizzeria_relazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaRelazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
